480a1ecd8067d8493c6de0aca96f3defbdefad0c
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-24 05:49:10 +0100

    Fix congfig file

--- front/src/config/index.js
+++ front/src/config/index.js
@@ -1,4 +1,4 @@
-export const RATES_WS_URL = 'ws://webtask.future-processing.com:8068/ws/currencies';
+export const RATES_WS_URL = 'wss://webtask.future-processing.com:8068/ws/currencies';
 
 export const FB_PROJECT_CONFIG = {
   apiKey: 'AIzaSyAavXuyFNA5tP4O3gWs4Bq29PNRPQUqYQ8',
@@ -9,12 +9,12 @@ export const FB_PROJECT_CONFIG = {
   messagingSenderId: '373756675850'
 };
 
-// const FB_FUNCTIONS_REGION = 'us-central1';
-// const FB_FUNCTIONS_FUNCTION_NAME = 'api';
-// const FB_PROJECT_NAME = 'online-exchange-app';
+const FB_FUNCTIONS_REGION = 'us-central1';
+const FB_FUNCTIONS_FUNCTION_NAME = 'api';
+const FB_PROJECT_NAME = 'online-exchange-app';
 
 // Deploy url
-// export const API_URL = `https://${FB_FUNCTIONS_REGION}-${FB_PROJECT_NAME}.cloudfunctions.net/${FB_FUNCTIONS_FUNCTION_NAME}`;
+export const API_URL = `https://${FB_FUNCTIONS_REGION}-${FB_PROJECT_NAME}.cloudfunctions.net/${FB_FUNCTIONS_FUNCTION_NAME}`;
 
 // Local url
-export const API_URL = 'http://localhost:5000/online-exchange-app/us-central1/api';
+// export const API_URL = 'http://localhost:5000/online-exchange-app/us-central1/api';
